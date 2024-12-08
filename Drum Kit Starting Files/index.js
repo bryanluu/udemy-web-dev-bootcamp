23826e6534b0c6b6f921a3848e4352d406d3303f
Bryan Luu <loc.bryan.luu@gmail.com>
2024-12-08 14:04:36 -0800

    :sparkles: make drums functional
    When clicked or key pressed, the appropriate sound is played.

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,65 @@
+function playSoundForDrum(drumKey) {
+  switch (drumKey) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play()
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play()
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play()
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play()
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play()
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play()
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play()
+      break;
+
+    default:
+      console.log(drumKey);
+      return;
+  }
+
+  console.log("A drum was played!")
+}
+
+function handleClick() {
+  var drumKey = this.innerHTML;
+  playSoundForDrum(drumKey);
+}
+
+var drums = document.querySelectorAll(".drum");
+
+document.querySelectorAll(".drum").forEach(
+  function (drum) {
+    drum.addEventListener("click", handleClick);
+  }
+);
+
+function handleKeyPress (keyEvent) {
+  var drumKey = keyEvent.key;
+  playSoundForDrum(drumKey);
+  console.log(drumKey + " was pressed!");
+}
+
+document.addEventListener("keydown", handleKeyPress);
