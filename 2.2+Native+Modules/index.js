5276a92f3cbe7e681c106db1efc6f5a7d174b36a
Bryan Luu <loc.bryan.luu@gmail.com>
2025-03-22 11:25:56 -0700

    :sparkles: complete challenge 2.2

--- 2.2+Native+Modules/index.js
+++ 2.2+Native+Modules/index.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt",  "Hello from NodeJS!", (err) => {
-  if (err) throw err;
-  console.log("The file has been saved!");
+// fs.writeFile("message.txt",  "Hello from NodeJS!", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
+
+fs.readFile("message.txt",
+  {"encoding": "utf-8"},
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
 });
