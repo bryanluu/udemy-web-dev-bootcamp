7ffc39d3557441a95206254ffd3cf361890d7f9e
Bryan Luu <loc.bryan.luu@gmail.com>
2025-03-23 15:24:04 -0700

    :sparkles: implement basic solution
    Next, add URL validation

--- 2.4+QR+Code+Project/2.4 QR Code Project/index.js
+++ 2.4+QR+Code+Project/2.4 QR Code Project/index.js
@@ -1,5 +1,31 @@
-/* 
+/*
 1. Use the inquirer npm package to get user input.
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import inquirer from "inquirer";
+import fs from "fs";
+import qr from "qr-image";
+import { log } from "console";
+
+inquirer
+  .prompt([{
+    name: "url",
+    message: "What's the URL to convert?"
+  }])
+  .then((answers) => {
+    fs.writeFile("url.txt", answers.url, (err) => {
+      if (err) throw err;
+
+      console.log("Raw URL written to url.txt");
+    });
+
+    let qr_png = qr.image(answers.url, { type: "png" });
+    qr_png.pipe(fs.createWriteStream("qr.png"));
+
+    console.log("QR Code saved to qr.png");
+  })
+  .catch((error) => {
+    throw error;
+  });
